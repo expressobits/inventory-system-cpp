b660cb97f9bd204700b78f4216858dbb2d50f132
Rafael Correa <scriptsengineer@gmail.com>
2023-01-19 20:03:34 -0300

    Update to godot 4 beta 13

--- src/inventory.h
+++ src/inventory.h
@@ -1,10 +1,6 @@
 #ifndef INVENTORY_CLASS_H
 #define INVENTORY_CLASS_H
 
-// We don't need windows.h in this example plugin but many others do, and it can
-// lead to annoying situations due to the ton of macros it defines.
-// So we include it and make sure CI warns us if we use something that conflicts
-// with a Windows define.
 #ifdef WIN32
 #include <windows.h>
 #endif
